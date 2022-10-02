edc21fb604baf5fd4c4ba264ca8d70d4df48cea4
tuesi <tadas.a.1793@gmail.com>
2022-10-02 18:43:26 +0300

    not bot

--- commands/debils.js
+++ commands/debils.js
@@ -10,7 +10,7 @@ module.exports = {
         .setName("kasyradebilas")
         .setDescription("Paklausk Zeniaus, kas yra servo debilas"),
         async execute(interaction) {
-            let user = interaction.guild.members.cache.filter(user => !user.bot).random().user;
+            let user = interaction.guild.members.cache.filter(user => user.bot === false).random().user;
             console.log(user);
             var trusabaka = emoji.getEmoji(interaction, "trusabaka");
             interaction.reply(`Sitam serve debilas yra ${user} ${trusabaka}`);
