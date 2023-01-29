60f97000a411b0a9231ea5b0291c7722f3a182f8
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2023-01-30 01:05:36 +0200

    zeniaus letorija

--- commands/zeniusloto.js
+++ commands/zeniusloto.js
@@ -1,7 +1,15 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const DiscordJS = require("discord.js");
 const emoji = require('../utils/emojiFinder');
-const wait = require('node:timers/promises').setTimeout;
+const fetch = require('node-fetch');
+
+function addLives(discordId) {
+    fetch(process.env.ADD_LIVES_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ secret: `${process.env.SECRET}`, discordId: `${discordId}` })
+    });
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,38 +21,33 @@ module.exports = {
             fetchReply: true
         });
 
-        // const emoji1 = emoji.getEmoji(interaction, "kiss~1");
-        // const emoji2 = emoji.getEmoji(interaction, "pirst");
-        // const emoji3 = emoji.getEmoji(interaction, "aurimts");
-        // const emoji4 = emoji.getEmoji(interaction, "trusabaka");
-        // const emoji5 = emoji.getEmoji(interaction, "fifidleiplaukas");
-        // const emoji6 = emoji.getEmoji(interaction, "ojtu");
-        // const emoji7 = emoji.getEmoji(interaction, "aurim2");
-        // const emoji8 = emoji.getEmoji(interaction, "chicago");
-        // const emoji9 = emoji.getEmoji(interaction, "ginispassport");
-        // const emoji10 = emoji.getEmoji(interaction, "ct2");
+        const emoji1 = emoji.getEmoji(interaction, "kiss~1");
+        const emoji2 = emoji.getEmoji(interaction, "pirst");
+        const emoji3 = emoji.getEmoji(interaction, "aurimts");
+        const emoji4 = emoji.getEmoji(interaction, "trusabaka");
+        const emoji5 = emoji.getEmoji(interaction, "fifidleiplaukas");
+        const emoji6 = emoji.getEmoji(interaction, "ojtu");
+        const emoji7 = emoji.getEmoji(interaction, "aurim2");
+        const emoji8 = emoji.getEmoji(interaction, "chicago");
+        const emoji9 = emoji.getEmoji(interaction, "ginispassport");
+        const emoji10 = emoji.getEmoji(interaction, "ct2");
 
-        const emoji1 = emoji.getEmoji(interaction, "sausginis");
-        const emoji2 = emoji.getEmoji(interaction, "trusabanis");
-        const emoji3 = emoji.getEmoji(interaction, "test1");
+        // const emoji1 = emoji.getEmoji(interaction, "sausginis");
+        // const emoji2 = emoji.getEmoji(interaction, "trusabanis");
+        // const emoji3 = emoji.getEmoji(interaction, "test1");
 
-        //console.log(emoji1.name);
-        //console.log(emoji2);
-        //console.log(emoji3);
-
-        //const emojiList = [emoji1, emoji2, emoji3, emoji4, emoji5, emoji6, emoji7, emoji8, emoji9, emoji10];
-        const emojiList = [emoji1, emoji2, emoji3];
+        const emojiList = [emoji1, emoji2, emoji3, emoji4, emoji5, emoji6, emoji7, emoji8, emoji9, emoji10];
+        //const emojiList = [emoji1, emoji2, emoji3];
 
         let emojiToUse = emojiList;
 
-        while (emojiToUse.length > 3) {
+        while (emojiToUse.length > 5) {
             let number = Math.floor(Math.random() * emojiList.length);
             emojiToUse.splice(number, 1);
         }
 
         const luckyNumber = Math.floor(Math.random() * emojiToUse.length);
         let luckyEmoji = emojiToUse[luckyNumber];
-        console.log(luckyEmoji.name);
 
         emojiToUse.forEach(emoji => {
             message.react(emoji);
@@ -57,12 +60,11 @@ module.exports = {
         const collector = message.createReactionCollector({ filter, time: 600000 });
 
         collector.on('collect', (reaction, user) => {
-            console.log(reaction.emoji.name);
-            console.log(luckyEmoji.name);
             if (reaction.emoji === luckyEmoji) {
-                interaction.followUp(`Laimejai blechamucha`);
+                interaction.followUp(`Laimejai blechamucha ir gavai 1 givybe dzimio kazino NACHER! www.debils.gay`);
+                addLives(interaction.user.id);
             } else {
-                interaction.followUp(`Bandyk dar karta vaikas`);
+                interaction.followUp(`Bandyk rytoj vaikas, pasaudysiu tau i strele kurva naxui!`);
             }
             collector.stop();
         });
