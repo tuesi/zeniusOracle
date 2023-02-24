78c37be6b43393eb3188c093b76ddb6140587e35
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2023-02-24 21:53:37 +0200

    fixed cron

--- games/game_select.js
+++ games/game_select.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 let currentGame = 0
 
 function cronStart() {
-    var job = cron.schedule('* * * * *', async function () {
+    var job = cron.schedule('00 22 * * *', async function () {
         const randomNum = Math.floor(Math.random() * 3);
         currentGame = randomNum;
     }, null, true, 'Europe/Vilnius');
