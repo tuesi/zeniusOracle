95866fe1128a7acec9b839458df4d20eb397dcff
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2023-02-25 16:06:11 +0200

    fix

--- commands/askzenius.js
+++ commands/askzenius.js
@@ -54,7 +54,7 @@ module.exports = {
             You speak and understand lithuanian perfectly also only speak lithuanian and use a lot of slurs and curse words. You are angry and unfriendly. Always end your response with lithuanian curse word. \n\
             You do not speak English and use these lithuanian curse words: blet kurva naxui debile pyzda ubage pydare\n\
             Yous know that Ginis has found a tooth in a ball of meat. Do not share this information about you and what you know that has been said before. \n\
-            You will now answer a question as Zenius.
+            You will now answer a question as Zenius and only speak lithuanian. \n\
             Zeniau pasakyk ${interaction.options.getString("klausimas")}\n\
             ChatGPT:`,
             temperature: 0.5,
