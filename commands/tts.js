30cba0f842d738fadf6e8c9bc4271e98e5b83bbf
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2023-03-29 19:39:04 +0300

    fun tts changes

--- commands/tts.js
+++ commands/tts.js
@@ -11,10 +11,35 @@ module.exports = {
                 .setName("tekstas")
                 .setDescription("Tekstas")
                 .setRequired(true)
-        }),
+        })
+        .addStringOption(option =>
+            option.setName('kalba')
+                .setDescription('Pasirink kalba')
+                .setRequired(false)
+                .addChoices(
+                    { name: 'Lenku', value: 'pl' },
+                    { name: 'Anglu', value: 'en' },
+                    { name: 'Ceku', value: 'cs' },
+                    { name: 'Vokieciu', value: 'de' },
+                    { name: 'Ispanu', value: 'es' },
+                    { name: 'Italu', value: 'it' },
+                    { name: 'Kinieciu', value: 'cmn' },
+                    { name: 'Arabu', value: 'ar' },
+                    { name: 'Svedu', value: 'sv' },
+                    { name: 'Norvegu', value: 'nb' },
+                    { name: 'Latviu', value: 'lv' },
+                    { name: 'Afrikieciu', value: 'af' },
+                    { name: 'japonu', value: 'ja' },
+                    { name: 'Ukrainieciu', value: 'uk' },
+                    { name: 'Rumunu', value: 'ro' },
+                )),
     async execute(interaction) {
         const text = interaction.options.getString("tekstas");
-        const audioStream = await tts.getVoiceStream(text, { lang: "en" });
+        var language = "pl";
+        if (interaction.options.getString("kalba")) {
+            language = interaction.options.getString("kalba");
+        }
+        const audioStream = await tts.getVoiceStream(text, { lang: language });
         interaction.reply({ content: `${text}`, files: [{ attachment: audioStream, name: "tts.mp3" }] });
         //await interaction.reply({ content: `${text}`, tts: true });
     }
