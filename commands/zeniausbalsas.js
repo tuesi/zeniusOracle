a9b256ee617a8e668785574347b61ab27dd3e552
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2023-03-31 00:21:20 +0300

    zeniaus ft max

--- commands/zeniausbalsas.js
+++ commands/zeniausbalsas.js
@@ -3,6 +3,7 @@ const { ButtonStyle, DiscordJS } = require("discord.js");
 const tts = require("discord-tts");
 const { Client, Intents } = require("discord.js");
 const { AudioPlayer, createAudioResource, StreamType, entersState, VoiceConnectionStatus, joinVoiceChannel, AudioPlayerStatus } = require("@discordjs/voice");
+const zeniusAudioEnum = require("../utils/zeniusAudioEnum.js");
 
 let audioPlayer;
 
@@ -38,6 +39,31 @@ module.exports = {
                     { name: 'Rumunu', value: 'ro' },
                 )),
     async execute(interaction) {
+
+        const tu_esi_sudas = createAudioResource('./assets/tu_esi_sudas.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const as_kasiaka_rukau = createAudioResource('./assets/AS_KASIAKA_RUKAU.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const as_tave_sveikinu = createAudioResource('./assets/as_tave_sveikinu.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const atsiradau = createAudioResource('./assets/atsiradaus.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const auksinis_pydaras = createAudioResource('./assets/AUKSINIS_PYD.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const balas_desimt = createAudioResource('./assets/BALAS_DESIMT.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const drakula = createAudioResource('./assets/DRAKULA.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const how_du_du = createAudioResource('./assets/How_di_du_du.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const kebabas = createAudioResource('./assets/kebabas.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const labai_malonu = createAudioResource('./assets/labai_malonu.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const manaroska = createAudioResource('./assets/manaroska.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const medis = createAudioResource('./assets/medis.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const myliu_tave = createAudioResource('./assets/myliu_tave.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const obuoly = createAudioResource('./assets/obuoly.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const plikai_sneka = createAudioResource('./assets/plikai_sneka.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const pydariau = createAudioResource('./assets/pydariau.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const sikni = createAudioResource('./assets/sikni.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const suesiu_tave = createAudioResource('./assets/suesiu_tave.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const tau_i_strele = createAudioResource('./assets/tau_i_strele.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const tava_pimpala = createAudioResource('./assets/pava_pimpala.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const tu_kaip_kriaukle = createAudioResource('./assets/TU_KAIP_KRIAUKLE.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const viena_pratyba = createAudioResource('./assets/viena_pratyba.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+        const zombis = createAudioResource('./assets/zombis.mp3', { inputType: StreamType.Arbitrary, inlineVolume: true });
+
         const text = truncate(interaction.options.getString("tekstas"), 100);
         var language = "pl";
         if (interaction.options.getString("kalba")) {
@@ -54,8 +80,83 @@ module.exports = {
         await interaction.reply({ content: '‎', ephemeral: true });
 
         try {
-            const audioStream = await tts.getVoiceStream(text, { lang: language });
-            const audioResource = createAudioResource(audioStream, { inputType: StreamType.Arbitrary, inlineVolume: true });
+            let audioResource;
+            switch (text) {
+                case zeniusAudioEnum.tu_esi_sudas:
+                    audioResource = tu_esi_sudas;
+                    break;
+                case zeniusAudioEnum.as_kasiaka_rukau:
+                    audioResource = as_kasiaka_rukau;
+                    break;
+                case zeniusAudioEnum.as_tave_sveikinu:
+                    audioResource = as_tave_sveikinu;
+                    break;
+                case zeniusAudioEnum.atsiradau:
+                    audioResource = atsiradau;
+                    break;
+                case zeniusAudioEnum.auksinis_pydaras:
+                    audioResource = auksinis_pydaras;
+                    break;
+                case zeniusAudioEnum.balas_desimt:
+                    audioResource = balas_desimt;
+                    break;
+                case zeniusAudioEnum.drakula:
+                    audioResource = drakula;
+                    break;
+                case zeniusAudioEnum.how_du_du:
+                    audioResource = how_du_du;
+                    break;
+                case zeniusAudioEnum.kebabas:
+                    audioResource = kebabas;
+                    break;
+                case zeniusAudioEnum.labai_malonu:
+                    audioResource = labai_malonu;
+                    break;
+                case zeniusAudioEnum.manaroska:
+                    audioResource = manaroska;
+                    break;
+                case zeniusAudioEnum.medis:
+                    audioResource = medis;
+                    break;
+                case zeniusAudioEnum.myliu_tave:
+                    audioResource = myliu_tave;
+                    break;
+                case zeniusAudioEnum.obuoly:
+                    audioResource = obuoly;
+                    break;
+                case zeniusAudioEnum.plikai_sneka:
+                    audioResource = plikai_sneka;
+                    break;
+                case zeniusAudioEnum.pydariau:
+                    audioResource = pydariau;
+                    break;
+                case zeniusAudioEnum.sikni:
+                    audioResource = sikni;
+                    break;
+                case zeniusAudioEnum.suesiu_tave:
+                    audioResource = suesiu_tave;
+                    break;
+                case zeniusAudioEnum.tau_i_strele:
+                    audioResource = tau_i_strele;
+                    break;
+                case zeniusAudioEnum.tava_pimpala:
+                    audioResource = tava_pimpala;
+                    break;
+                case zeniusAudioEnum.tu_kaip_kriaukle:
+                    audioResource = tu_kaip_kriaukle;
+                    break;
+                case zeniusAudioEnum.viena_pratyba:
+                    audioResource = viena_pratyba;
+                    break;
+                case zeniusAudioEnum.zombis:
+                    audioResource = zombis;
+                    break;
+                default:
+                    const audioStream = await tts.getVoiceStream(text, { lang: language });
+                    audioResource = createAudioResource(audioStream, { inputType: StreamType.Arbitrary, inlineVolume: true });
+                    break;
+            }
+
             if (!voiceConnection || voiceConnection?.status === VoiceConnectionStatus.Disconnected) {
                 voiceConnection = joinVoiceChannel({
                     channelId: interaction.member.voice.channelId,
@@ -64,7 +165,6 @@ module.exports = {
                 });
                 voiceConnection = await entersState(voiceConnection, VoiceConnectionStatus.Connecting, 5_000);
             }
-
             if (voiceConnection.status === VoiceConnectionStatus.Connected) {
                 voiceConnection.subscribe(audioPlayer);
                 audioPlayer.play(audioResource);
@@ -74,7 +174,8 @@ module.exports = {
                     }
                 });
             }
-        } catch {
+        } catch (e) {
+            console.log(e);
             console.log("Something whent wrong");
         }
     }
---
+++ utils/zeniusAudioEnum.js
@@ -0,0 +1,28 @@
+const zeniusAudioEnum = {
+    tu_esi_sudas: "tu esi sudas",
+    as_kasiaka_rukau: "as kasiaka rukau",
+    as_tave_sveikinu: "as tave sveikinu",
+    atsiradau: "atsiradau",
+    auksinis_pydaras: "auksinis pydatas",
+    balas_desimt: "balas desimt",
+    drakula: "drakula",
+    how_du_du: "how du du",
+    kebabas: "kebabas",
+    labai_malonu: "labai man malonu",
+    manaroska: "manaroska",
+    medis: "medis",
+    myliu_tave: "myliu tave",
+    obuoly: "obuolys",
+    plikai_sneka: "plikai sneka",
+    pydariau: "pydariau",
+    sikni: "sikni",
+    suesiu_tave: "suesiu tave",
+    tau_i_strele: "pasaudysiu tau i strele",
+    tava_pimpala: "tavo pimpala",
+    tu_kaip_kriaukle: "tu kaip kriaukle",
+    viena_pratyba: "viena pratyba",
+    zombis: "zombis"
+}
+
+
+module.exports = zeniusAudioEnum;
