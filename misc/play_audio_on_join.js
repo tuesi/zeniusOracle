c8a873e5b9822eebbfd76f598789e30b435b07e9
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2023-04-18 00:15:38 +0300

    maksas new sound on user join

--- misc/play_audio_on_join.js
+++ misc/play_audio_on_join.js
@@ -2,11 +2,16 @@ const { AudioPlayer, entersState, joinVoiceChannel, createAudioResource, AudioPl
 
 module.exports = (client) => {
     client.on('voiceStateUpdate', async (oldState, newState) => {
-        if (newState.member.user.id === '305028139514593291' && newState.channel) {
+        if ((newState.member.user.id === '305028139514593291' || newState.member.user.id === '198493904444588032') && newState.channel) {
             const audioPlayer = new AudioPlayer();
             let voiceConnection;
             try {
-                const audioResource = createAudioResource('./assets/obuoly.mp3');
+                let audioResource;
+                if (newState.member.user.id === '305028139514593291') {
+                    audioResource = createAudioResource('./assets/as_vel_sugrizau.mp3');
+                } else if (newState.member.user.id === '198493904444588032') {
+                    audioResource = createAudioResource('./assets/batarke_baigias.mp3');
+                }
 
                 if (!voiceConnection || voiceConnection?.status === VoiceConnectionStatus.Disconnected) {
                     voiceConnection = joinVoiceChannel({
@@ -23,12 +28,20 @@ module.exports = (client) => {
                     audioPlayer.on(AudioPlayerStatus.Idle, () => {
                         voiceConnection.destroy();
                     });
+
                     voiceConnection.on(VoiceConnectionStatus.Disconnected, () => {
                         audioPlayer.stop();
+                        if (voiceConnection.state.status !== VoiceConnectionStatus.Destroyed) {
+                            voiceConnection.destroy();
+                        }
                     });
                 }
             } catch (e) {
-                voiceConnection.disconnect();
+                if (voiceConnection) {
+                    if (voiceConnection.state.status !== VoiceConnectionStatus.Destroyed) {
+                        voiceConnection.disconnect();
+                    }
+                }
                 audioPlayer.stop();
                 console.log(e);
                 console.log("Something whent wrong");
